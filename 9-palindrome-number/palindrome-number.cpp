e65da8c65a0ce7d5f974c5c96d2e13a805f90d39
Salman <89701712+Itssalman095@users.noreply.github.com>
2026-08-12 19:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.75%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending in 0 (except 0 itself) can't be palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reverted = 0;
+        while (x > reverted) {
+            reverted = reverted * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Even digit count: x == reverted
+        // Odd digit count: middle digit is discarded via reverted / 10
+        return x == reverted || x == reverted / 10;
+    }
+};
